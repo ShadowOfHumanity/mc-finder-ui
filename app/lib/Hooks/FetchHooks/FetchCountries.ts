90c4766f3c6e5b18f8f0b0edbc7d3a4484f689ef
darian baker bray <darianbakerbray@gmail.com>
2025-12-03 18:15:13 +0100

    finished country fetcher

--- app/lib/Hooks/FetchHooks/FetchCountries.ts
+++ app/lib/Hooks/FetchHooks/FetchCountries.ts
@@ -1,2 +1,21 @@
 
 // we need a API object.. axios..but i need one for OUR api & other APIs.. i dont want to rewrite calls!
+// https://restcountries.com/v3.1/all
+
+import { Country } from "../../types/Country";
+import { createAxiosInstance } from "../../util/AxiosInstance";
+
+const countryApi = createAxiosInstance("https://restcountries.com/v3.1/") 
+export const fetchCountries = async () => {
+    try {
+        const response = await countryApi.get<Country[]>("/all", {
+            params: {
+                fields: "name, population, cca2, flag"
+            }
+        })
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching countries:", error);
+        throw error;
+    }
+}''
--- app/lib/types/Country.ts
+++ app/lib/types/Country.ts
@@ -1,6 +1,17 @@
 export interface Country {
-    name: string;
+    name: CountryName;
     population: number;
     cca2: string; // cca2 is the country code
     flag: string;
+}
+
+export interface CountryName {
+    common: string;
+    official: string;
+    nativeName: {
+        [key: string]: { // this is to get in a specific language, in this case always english for example.
+            common: string;
+            official: string;
+        }
+    }
 }
